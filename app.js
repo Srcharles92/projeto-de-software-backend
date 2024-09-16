384bdb00a0a5a49f581bbad7746767ea02ac19f2
Srcharles92 <charles.guerra56@gmail.com>
2024-09-16 19:56:38 -0300

    atualizacao-funcional1

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
 const express = require('express');
 const livrosRouter = require('./routes/livraria.routes');
 const connection = require('./configDb/db');
+const cors = require('cors') 
+const bodyParser = require('body-parser')
+
 
 const app = express();
 const port = 4000;
 
 
 app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true})); // Configuração para usar parâmetros na URL.
 app.use('/api', livrosRouter); // Base da URL para as rotas dos livros
 
 // conecta com o banco e mostra se deu erro.
